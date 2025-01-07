081e4a148fdf7ea7d52d56afac2f35f706b5f3d4
AlkahafM <alkahafmukadam2@gmail.com>
2025-01-07 12:07:33 +0530

    Add Category Controller & Category view

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookly.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
