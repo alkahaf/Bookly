91c632c21e7d0d9f1933cd2ee4a09d7939139cfa
AlkahafM <alkahafmukadam2@gmail.com>
2025-01-07 13:04:36 +0530

    Display Category table on website

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bookly.Data;
+using Bookly.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bookly.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) {
+        _db = db;   
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Category.ToList();
             return View();
         }
     }
