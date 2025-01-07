9edd8389f5d79d4fab6f3b561e526b94796f147c
AlkahafM <alkahafmukadam2@gmail.com>
2025-01-07 11:54:39 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bookly.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookly.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //connection string
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options ): base(options) 
+        {
+                    
+        }
+        public  DbSet<Category> Category { get; set; }
+    }
+}
