348aca1812be165dd4217ae7fbc85a3ef530ddd1
AlkahafM <alkahafmukadam2@gmail.com>
2025-01-08 12:42:03 +0530

    Add TempData

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -31,6 +31,7 @@ namespace Bookly.Controllers
             {
                 _db.Category.Add(obj);
                 _db.SaveChanges();
+                TempData["success"]="Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -59,6 +60,8 @@ namespace Bookly.Controllers
             {
                 _db.Category.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+
                 return RedirectToAction("Index");
             }
             return View();
@@ -89,6 +92,8 @@ namespace Bookly.Controllers
             }
             _db.Category.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+
             return RedirectToAction("Index");
          
         }
