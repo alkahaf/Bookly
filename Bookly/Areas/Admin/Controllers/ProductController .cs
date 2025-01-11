0fd42c3391bd76adbf1a599189b2e5807de9fb8c
AlkahafM <alkahafmukadam2@gmail.com>
2025-01-11 16:42:36 +0530

    Enable to Create Product

--- Bookly/Areas/Admin/Controllers/ProductController .cs
+++ Bookly/Areas/Admin/Controllers/ProductController .cs
@@ -2,6 +2,7 @@
 using Bookly.DataAccess.Data;
 using Bookly.DataAccess.Repository.IRepository;
 using Bookly.Models;
+using Bookly.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -25,27 +26,43 @@ namespace Bookly.Areas.Admin.Controllers
         }
         public IActionResult Create()
         {
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
-            {
-                Text = u.Name,
-                Value = u.Id.ToString()
-            });
+           
+            ProductVM productVM = new() 
+            { 
+                CategoryList = _unitOfWork.Category
+                .GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product()
+            };
 
-            ViewBag.CategoryList = CategoryList;
-            return View();
+            return View(productVM);
         }
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Create(ProductVM productVM)
         {
 
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
-            return View();
+            else
+            {
+
+                productVM.CategoryList = _unitOfWork.Category
+               .GetAll().Select(u => new SelectListItem
+               {
+                   Text = u.Name,
+                   Value = u.Id.ToString()
+               });
+                
+                return View(productVM);
+            }
         }
 
         public IActionResult Edit(int? id)
