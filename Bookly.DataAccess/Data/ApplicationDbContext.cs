243789202eb9f76f9bc05a4ee4900125630f0bc1
AlkahafM <alkahafmukadam2@gmail.com>
2025-01-23 18:57:35 +0530

    Integrate ASP.NET Core Identity
    Updated project files to include necessary Identity packages.
    Modified `ApplicationDbContext` to inherit from `IdentityDbContext`.
    Added Razor Pages for identity management (login, registration, 2FA, etc.).
    Implemented user authentication and account management features.
    Enhanced error handling and validation scripts.
    Configured default identity services in `Program.cs`.
    Updated `appsettings.json` with new connection strings.

--- Bookly.DataAccess/Data/ApplicationDbContext.cs
+++ Bookly.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Bookly.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookly.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext :  IdentityDbContext
     {
         //connection string
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options ): base(options) 
@@ -15,6 +17,8 @@ namespace Bookly.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
