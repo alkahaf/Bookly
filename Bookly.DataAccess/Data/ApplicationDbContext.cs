082236999366d0d2014b2c0d07bb57afc2166f40
AlkahafM <alkahafmukadam2@gmail.com>
2025-01-24 11:01:57 +0530

    Update project structure and clean up ApplicationDbContext
    - Include new 'Data' folder in Bookly.DataAccess.csproj.
    - Specify IdentityUser as generic type in ApplicationDbContext.
    - Remove redundant usings and OnModelCreating method.
    - Include new 'Areas\Identity\Data' folder in Bookly.csproj.

--- Bookly.DataAccess/Data/ApplicationDbContext.cs
+++ Bookly.DataAccess/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Bookly.DataAccess.Data
 {
-    public class ApplicationDbContext :  IdentityDbContext
+    public class ApplicationDbContext :  IdentityDbContext<IdentityUser>
     {
         //connection string
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options ): base(options) 
