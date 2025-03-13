d7886034d524c9cb97c345a877c8c75d8a9dc250
AlkahafM <alkahafmukadam2@gmail.com>
2025-03-13 07:48:44 +0530

    #Dropdown of Company is added

--- Bookly.Models/ApplicationUser.cs
+++ Bookly.Models/ApplicationUser.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 //using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace Bookly.Models
 {
@@ -17,6 +19,10 @@ namespace Bookly.Models
         public string? City { get; set; }
         public string? State{ get; set; }
         public string? PostalCode{ get; set; }
+        public int? CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
+        [ValidateNever]
+        public Company Company { get; set; }
         
     }
 }
