cdb51b84465dfcd41ac6aa497f3f5fe869ddcb27
AlkahafM <alkahafmukadam2@gmail.com>
2025-03-13 14:16:37 +0530

    Add ApplicationUser Repository

---
+++ Bookly.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Bookly.DataAccess.Data;
+using Bookly.DataAccess.Repository.IRepository;
+using Bookly.Models;
+
+namespace Bookly.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+     
+
+    }
+}
